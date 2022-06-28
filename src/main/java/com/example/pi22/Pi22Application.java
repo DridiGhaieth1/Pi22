651f3f36c81f99810008ca046bb25a9019233929
Ghaieth Dridi <ghaieth.dridi@esprit.tn>
2022-06-28 11:32:22 +0100

    Add all files

---
+++ src/main/java/com/example/pi22/Pi22Application.java
@@ -0,0 +1,13 @@
+package com.example.pi22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pi22Application.class, args);
+    }
+
+}
